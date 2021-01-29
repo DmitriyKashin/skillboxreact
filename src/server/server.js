d4f7f866bd13e29a787b319e6c72532de6dd5ea8
Кашин Дмитрий <wild.mephist@gmail.com>
2021-01-29 17:11:34 +0100

    fxes for production

--- src/server/server.js
+++ src/server/server.js
@@ -20,9 +20,9 @@ app.get('/auth', (req, res) => {
   console.log(req.query.code);
   axios.post(
     'https://www.reddit.com/api/v1/access_token',
-    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.DOMAIN}/auth`,
     {
-      auth: { username: process.env.CLIENT_ID, password:'NUYRbIcVy7K9tG7nYcog6f7UqACPiA'},
+      auth: { username: process.env.CLIENT_ID, password:process.env.SECRET},
       headers: {'Content-type': 'application/x-www-form-urlencoded'}
     }
   ).then(({data}) => {
@@ -45,5 +45,5 @@ app.get('*', (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Server just have started on http://localhost:${PORT}`);
+  console.log(`Server just have started on ${process.env.DOMAIN}:${PORT}`);
 });
